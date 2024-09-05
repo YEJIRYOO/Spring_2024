d7fa02b3c383723c5ced1340b3720fbda47ac2b5
YeJiRyoo <yejiryoo@YeJiRyooui-MacBookPro.local>
2024-09-05 23:55:42 +0900

    feat: Appconfig

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,6 +11,11 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository=new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy=new RateDiscountPolicy();
+    /*
+    OCP, DIP 문제 발생 : OrderService가 인터페이스 뿐만 아니라 구현에도 의존!
+    -> 기능 확장하여 변경 시, 클라이언트 코드에 영향을 미침
+     */
 
     @Override
     public Order createOrder(Long memberId,String itemName,int itemPrice){
