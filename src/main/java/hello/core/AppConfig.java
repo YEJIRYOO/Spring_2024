79ee6ff3a4f5e0eb5f97e2ac9dde7918efed564f
YeJiRyoo <yejiryoo@YeJiRyooui-MacBookPro.local>
2024-09-08 16:41:11 +0900

    fix: AppConfig Refactoring_ delete code repetition

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,9 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -17,12 +17,12 @@ import hello.core.order.OrderServiceImpl;
 //애플리케이션의 실제 동작에 필요한 구현 객체 생성
 public class AppConfig {
 
+    /*
+    // BEFORE 리팩터링: 역할과 구분이 잘 드러나지 않는 상태, 코드 중복 존재
+
     public MemberService memberService(){
         //생성자 주입
         return new MemberServiceImpl(new MemoryMemberRepository());
-        /*
-        구현 클래스에서 생성자 설정 필요
-         */
     }
 
     public OrderService orderService(){
@@ -31,4 +31,31 @@ public class AppConfig {
                 new MemoryMemberRepository(),
                 new FixDiscountPolicy());
     }
+     */
+
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+    //-> 현재 나의 애플리케이션에서 MemberRepos 로는 MemorymemberRepos 를 사용하고,
+    //   DiscountPolicy 로는 FixDiscountPolicy 를 사용함을 한 눈에 알 수 O
+    //   향후 수정 시 생성 부분만 수정하면 됨
+
+    public MemberService memberService(){
+        //생성자 주입
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService(){
+        //구현체 반환
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //-> 현재 내 애플리케이션에서 사용하는 memberRepository 와 discountPolicy 를 사용할거야
+
 }
