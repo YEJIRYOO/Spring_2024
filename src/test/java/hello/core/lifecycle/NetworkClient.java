bacad20ed6ff715c27b6e10322d595abe4b1bbb5
YEJIRYOO <emilyryoo5307@gmail.com>
2024-10-20 19:59:23 +0900

    CallBack1: Interface

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,20 +1,22 @@
 package hello.core.lifecycle;
 
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
-        System.out.println("생성자 호출, url="+url);
-        connect();
-        call("초기화 연결 메시지");
+        System.out.println("생성자 호출, url= "+url);
     }
 
     public void setUrl(String url){
         this.url=url;
     }
 
+    //서비스 시작시 호출
     public void connect(){
         System.out.println("connect: "+url);
     }
@@ -23,7 +25,21 @@ public class NetworkClient {
         System.out.println("call: "+url+" message= "+message);
     }
 
+    //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: "+url);
+    }
+
+    //InitializingBean의 메서드 초기화 지원 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //DisposableBean의 메서드 소멸 지원 메서드
+    @Override
+    public void destroy() throws Exception{
+        disconnect();
     }
 }
