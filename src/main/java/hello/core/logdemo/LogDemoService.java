24241cfade352e3f46477cad344d4daacc0766e2
YEJIRYOO <emilyryoo5307@gmail.com>
2024-10-26 21:27:37 +0900

    sol1: Object Provider- delay generation

--- src/main/java/hello/core/logdemo/LogDemoService.java
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -2,15 +2,22 @@ package hello.core.logdemo;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
+    /*
     private final MyLogger myLogger;
+     */
+
+    //sol1: ObjectProvider
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id){
+        MyLogger myLogger= myLoggerProvider.getObject();
         myLogger.log("service id="+id);
     }
 }
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ import hello.core.common.MyLogger;
 import hello.core.logdemo.LogDemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+
+    /*
     private final MyLogger myLogger; //request scope
+    //문제 상황: request scope의 myLogger와 singleton의 요청 시점 불일치
+     */
+
+    //sol1: ObjectProvider
+    //ObjectProvider 통해 getObject() 호출 시점까지 request scope 빈의 생성 지연
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody //뷰 없이 문자 반환
     public String logDemo(HttpServletRequest request){
         String requestURL=request.getRequestURL().toString();
+        MyLogger myLogger= myLoggerProvider.getObject(); //sol1:ObjectProvider
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
